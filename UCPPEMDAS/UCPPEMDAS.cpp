a248c275646661923d4af369f89d8fc90bec5eda
M S I <M S I@MSI>
2024-04-29 10:31:01 +0700

    Add project files.

---
+++ UCPPEMDAS/UCPPEMDAS.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	for (int i = 0; i < 20; i++) {
+		string nama;
+		double nilaimatematika, nilaiinggris;
+
+		cout << "Masukkan Nama Mahasiswa: ";
+		cin >> nama;
+		cout << "Masukkan nilai matematika: ";
+		cin >> nilaimatematika;
+		cout << "Masukkan nilai inggrisnya: ";
+		cin >> nilaiinggris;
+	}
+}
+
+
+
+// 1. intejer = int umur = 19;
+//    string = string nama;
+// 2. if,while
+// 3. terdiri dari beberapa variable seperti string,integer
+// 4. void
+// 5. for,do while,
+
